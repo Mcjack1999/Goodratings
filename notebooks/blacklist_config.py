84bfd0df7a485417c1ca0758f955c2d7a732533c
mcjack1999 <mjack245@pratt.edu>
2025-05-06 20:09:42 -0400

    added config files

---
+++ notebooks/blacklist_config.py
@@ -0,0 +1,50 @@
+blacklist = [
+    # 1. Reading status
+    'to-read', 'read-in-2016', 'currently-reading', 'tbr', 'read-2016', 'read-2015', 'read-2014',
+    'read-2013', 'read-2012', 'read-2011', 'read-2010', 're-read', 'reread', 'to-reread', 'already-read',
+    'read-again', 'read-aloud', 'read-alouds', 'read-fiction', 'read-in-school', 'read-as-a-kid',
+    'read-comics', 'read-next', 'read-2009', 'read-in-english',
+
+    # 2. Ownership/location
+    'owned', 'my-books', 'library', 'kindle', 'ebooks', 'books-i-own', 'to-buy', 'owned-books',
+    'i-own', 'my-library', 'own-it', 'borrowed', 'on-my-shelf', 'books-i-have', 'bookshelf',
+    'home-library', 'my-bookshelf', 'own-to-read', 'own-ebook', 'my-ebooks', 'owned-tbr',
+    'ebooks-i-own', 'own-kindle', 'owned-not-read', 'owned-but-not-read', 'own-on-kindle',
+    'kindle-owned', 'own-unread', 'owned-unread', 'owned-ebook', 'own-a-copy', 'my-kindle-books',
+    'owned-kindle', 'own-tbr', 'kindle-library', 'books-owned', 'kindle-to-read', 'not-owned',
+    'do-not-own', 'owned-not-read', 'owned-but-not-read', 'own-unread', 'own-a-copy',
+
+    # 3. Format or device
+    'audiobook', 'ebook', 'paperback', 'hardcover', 'audiobooks', 'audio', 'e-book', 'e-books',
+    'audio-books', 'audio-book', 'audible', 'library-book', 'kindle-books', 'nook', 'on-kindle',
+    'netgalley', 'overdrive', 'pdf', 'epub', 'ibooks', 'kobo', 'kindle-unlimited', 'digital',
+    'graphic', 'ebook-owned', 'epub', 'e-reader', 'calibre', 'ibooks', 'kobo', 'downloaded',
+    'kindle-book', 'ebooks-i-own',
+
+    # 4. Rating/review-based
+    '5-stars', 'favorites', 'favourites', 'favorite', 'favorite-books', 'favorite-series',
+    'my-favorites', 'favorite-authors', 'favorite-author', 'faves', 'favourite', 'fav',
+    'gave-up-on', 'gave-up', 'did-not-finish', 'dnf', 'abandoned', 'unfinished', 'didn-t-finish',
+    'couldn-t-finish', 'could-not-finish', 'not-finished', 'not-read', 'never-finished', 'paused',
+    'stopped-reading', 'not-for-me', 'nope', 'meh', 'dnf', 'review', 'reviewed',
+
+    # 5. Challenge or year-based
+    '2016-reading-challenge', '2017-reading-challenge', '2015-reading-challenge', '2014-read',
+    '2015-reads', '2014-reads', '2013-reads', '2012-reads', '2011-reads', '2010-reads', '2016-reads',
+    '2015-books', '2014-books', '2013-books', '2012-books', '2017-reads', '2017-books',
+    '2017-read', '2017-release', '2017-reading-list', '2017-books-read', 'books-read-in-2016',
+    'books-read-in-2015', 'books-read-in-2014', 'books-read-in-2013', 'books-read-in-2012',
+    'books-read-in-2017', 'books-read-in-2011', 'books-read-in-2010', 'read-in-2015',
+    'read-in-2014', 'read-in-2013', 'read-in-2012', 'read-in-2011', 'read-in-2010', '2016-books',
+    '2016-read', '2017-books-read', '2017-books-read', 'books-read-in-2016', '2016-books-read',
+
+    # 6. Meta or personal tags
+    'wishlist', 'wish-list', 'book-club', 'bookclub', 'book-club-books', 'book-club-reads',
+    'book-group', 'to-re-read', 're-read', 'reread', 'to-reread', 'to-read-owned', 'to-read-own',
+    'to-read-fiction', 'to-read-non-fiction', 'to-read-nonfiction', 'to-read-ya', 'to-read-series',
+    'to-read-classics', 'to-read-fantasy', 'to-review', 'to-purchase', 'to-be-read', 'to-be-released',
+    'to-read-soon', 'to-get', 'want-to-read', 'want-to-buy', 'want', 'need', 'need-to-buy',
+    'need-to-get', 'not-interested', 'maybe', 'maybe-read', 'recommendations', 'next', 'next-to-read',
+    'next-in-series', 'done', 'shelved', 'my-shelf', 'own-a-copy', 'own-tbr', 'top-tbr', 'not-for-me',
+    'considering', 'owned-but-not-read', 'wishlist', 'wishlist', 'owned-to-read'
+]
